f369c79e7427aee73c593c3f2b1344aaf7e1377b
goosetea04 <gusti.fatu@gmail.com>
2023-11-09 10:35:46 +0700

    Tutorial 7

---
+++ lib/screens/menu.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/widgets/shop_card.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<ShopItem> items = [
+    ShopItem("View Products", Icons.checklist),
+    ShopItem("Add Product", Icons.add_shopping_cart),
+    ShopItem("Logout", Icons.logout),
+  ];
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Shopping List',
+        ),
+      ),
+      body: SingleChildScrollView(
+        // Scrolling wrapper widget
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding for the page
+          child: Column(
+            // Widget to display children vertically
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Text widget to display text with center alignment and appropriate style
+                child: Text(
+                  'PBP Shop', // Text indicating the shop name
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container for our cards.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ShopItem item) {
+                  // Iteration for each item
+                  return ShopCard(item);
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
+      drawer: const LeftDrawer(),
+    );
+  }
+}
